0b31510c1075415b3f128c3734a91abcc0955f26
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-07-13 15:08:20 -0300

    Corrige deploy

--- github-users-redux/src/App.js
+++ github-users-redux/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Home from './components/Home';
 import React, { Component } from 'react'
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Users from './components/Users';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
+        <BrowserRouter basename="/github-user-finder">
           <Switch>
             <Route exact path="/" component={ Home } />
             <Route exact path="/user/:user" component={ Users } />
